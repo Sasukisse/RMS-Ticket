8c8a818a129a2abbca43cf93950cf0299d2a6534
Sasukisse <samuel.tardy78@gmail.com>
2026-02-02 16:18:23 +0100

    Correction des paramètres de connexion MySQL dans le fichier de configuration. Réinitialisation de l'hôte, de l'utilisateur et du mot de passe par défaut.

--- Database/config.php
+++ Database/config.php
@@ -1,7 +1,7 @@
 <?php
-// Param�tres de connexion MySQL (XAMPP par d�faut). Modifiez DB_NAME, DB_USER, DB_PASS selon votre setup.
-const DB_HOST = '5.135.83.195';
+// Paramètres de connexion MySQL (XAMPP par défaut). Modifiez DB_NAME, DB_USER, DB_PASS selon votre setup.
+const DB_HOST = '127.0.0.1';
 const DB_NAME = 'rms_ticket';
-const DB_USER = 'sanjivy';
-const DB_PASS = 'sanjivy78';
+const DB_USER = 'root';
+const DB_PASS = '';
 const DB_CHARSET = 'utf8mb4';
